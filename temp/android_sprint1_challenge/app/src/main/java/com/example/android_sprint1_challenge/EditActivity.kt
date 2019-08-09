465d8a36253fc59a5768b6ae82079c17171a9b95
GCGsauce <gattigcg1@gmail.com>
2019-08-09 12:40:45 -0400

    xml design pretty much done

--- temp/android_sprint1_challenge/app/src/main/java/com/example/android_sprint1_challenge/EditActivity.kt
+++ temp/android_sprint1_challenge/app/src/main/java/com/example/android_sprint1_challenge/EditActivity.kt
@@ -1,12 +1,21 @@
 package com.example.android_sprint1_challenge
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit.*
 
 class EditActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
+
+        save_button.setOnClickListener {
+            val text = movie_text.getText().toString()
+            if(text == ""){
+                val intent = Intent()
+            }
+        }
     }
 }
--- temp/android_sprint1_challenge/app/src/main/java/com/example/android_sprint1_challenge/MainActivity.kt
+++ temp/android_sprint1_challenge/app/src/main/java/com/example/android_sprint1_challenge/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.android_sprint1_challenge
 
 import android.content.Intent
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +18,18 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, EditActivity::class.java)
             this.startActivity(intent)
         }
+    }
 
+    fun createTextView(content : String, has_been_watched : Boolean) : TextView{
+        val text_view = TextView(this)
+        text_view.text = content
+        text_view.textSize = 24f
+        text_view.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+
+        if(has_been_watched){
+            text_view.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG)
+        }
+
+        return text_view
     }
 }
