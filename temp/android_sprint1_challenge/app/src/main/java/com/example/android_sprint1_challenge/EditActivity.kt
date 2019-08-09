35f8a902392347df221da9de50a0214fb4ccc1a6
GCGsauce <gattigcg1@gmail.com>
2019-08-09 14:07:16 -0400

    xml design pretty much done

--- temp/android_sprint1_challenge/app/src/main/java/com/example/android_sprint1_challenge/EditActivity.kt
+++ temp/android_sprint1_challenge/app/src/main/java/com/example/android_sprint1_challenge/EditActivity.kt
@@ -1,5 +1,6 @@
 package com.example.android_sprint1_challenge
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,15 +8,42 @@ import kotlinx.android.synthetic.main.activity_edit.*
 
 class EditActivity : AppCompatActivity() {
 
+    companion object{
+        const val MOVIE_ENTRY = "1"
+        const val WATCHED_OR_NOT = "2"
+        const val DELETE_MOVIE = "3"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
 
         save_button.setOnClickListener {
-            val text = movie_text.getText().toString()
-            if(text == ""){
-                val intent = Intent()
-            }
+            onBackPressed()
         }
+
+        delete_button.setOnClickListener {view ->
+            val movie = movie_text.text
+            val intent = Intent()
+            intent.putExtra(DELETE_MOVIE, movie)
+
+            setResult(Activity.RESULT_OK, intent)
+
+            finish()
+        }
+    }
+
+    override fun onBackPressed() {
+        val movie = movie_text.text
+        val watched = watched_or_not.isChecked()
+
+        val intent = Intent()
+
+        intent.putExtra(MOVIE_ENTRY, movie)
+        intent.putExtra(WATCHED_OR_NOT, watched)
+
+        setResult(Activity.RESULT_OK, intent)
+
+        finish()
     }
 }
--- temp/android_sprint1_challenge/app/src/main/java/com/example/android_sprint1_challenge/MainActivity.kt
+++ temp/android_sprint1_challenge/app/src/main/java/com/example/android_sprint1_challenge/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.android_sprint1_challenge
 
+import android.app.Activity
 import android.content.Intent
 import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
@@ -10,13 +11,17 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    companion object{
+        const val MOVIE_REQUEST = 654
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         movie_button.setOnClickListener {
             val intent = Intent(this, EditActivity::class.java)
-            this.startActivity(intent)
+            startActivityForResult(intent, MOVIE_REQUEST)
         }
     }
 
@@ -30,6 +35,41 @@ class MainActivity : AppCompatActivity() {
             text_view.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG)
         }
 
+        text_view.setOnClickListener { view ->
+            movie_layout.removeView(text_view)
+            val intent = Intent(this, EditActivity::class.java)
+            startActivityForResult(intent, MOVIE_REQUEST)
+        }
+
+        movie_button.setOnClickListener { view->
+            val intent = Intent(this, EditActivity::class.java)
+            startActivityForResult(intent, MOVIE_REQUEST)
+        }
+
         return text_view
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(resultCode == Activity.RESULT_OK){
+            when(requestCode){
+                MOVIE_REQUEST -> {
+                    val text = data?.getStringExtra(EditActivity.MOVIE_ENTRY)
+                    val watched = data?.getBooleanExtra(EditActivity.WATCHED_OR_NOT, false)
+                    val delete = data?.getStringExtra(EditActivity.DELETE_MOVIE)
+
+                    if(text != null && !text.equals("") && watched != null){
+                        movie_layout.addView(createTextView(text, watched))
+                    } else if(delete != null && !delete.equals("")){
+                        for (x in 0..(movie_layout.childCount)){
+                            val t_view : TextView = movie_layout.getChildAt(x) as TextView
+                            if(t_view.text.equals(delete)){
+                                movie_layout.removeViewAt(x)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
